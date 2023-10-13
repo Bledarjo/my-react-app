c8399b01676838185fabf7efe734be15cf0c919d
Bledarjo <j.dufag@gmail.com>
2023-10-13 15:50:39 +0200

    state.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,63 @@
+import { useState } from "react";
+
 import PokemonCard from './components/PokemonCard'
-// import './App.css'
+import './App.css'
+
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 
 
 
 function App() {
+  const [pokemonIndex,setpokemonIndex]=useState(0);
   
+ const [disabled,setdisabled]=useState(false)
 
+  const handlePreviousClick =() =>{
+    setpokemonIndex((pokemonList)=>pokemonList-1)
+    // if (disabled == pokemonList[0]){
+    // setdisabled(true)
+  }
+     
+  const handleNextClick=()=>{
+    setpokemonIndex((pokemonList)=>pokemonList+1)}
+    // if(setdisabled== pokemonList[4]){
+    //   setdisabled(true)
+    
+   
   return (
     
       <div>
-       
-        <PokemonCard pokemon={pokemonList[0]}/>
-        </div>
+        <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+       <button  onClick={handlePreviousClick} disabled={pokemonIndex<=0}>Prècedent</button>
+       <button  onClick={handleNextClick} disabled={pokemonIndex>=4}>Suivant</button>
+        
+      </div>
        
     
   );
